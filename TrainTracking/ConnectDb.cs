63327709a9132853d1d6f61318eaf14b07362fea
MArslanRiaz96 <Arslanriaz96developer@gmail.com>
2019-05-11 21:50:38 +0500

    add connect db class

---
+++ TrainTracking/ConnectDb.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainTracking
+{
+   public class ConnectDb
+    {
+       public static string connectionString = "Data Source=.;Initial Catalog=traintracker;Integrated Security=True; MultipleActiveResultSets=true";
+      // public static string connectionString = "Data Source = den1.mssql8.gear.host;  Initial Catalog= traintracker;  Uid=traintracker;  Pwd=Yy82!l6X6NU-; MultipleActiveResultSets=true";
+    //  public static string connectionString = "Data Source = den1.mssql8.gear.host;  Initial Catalog= traintracker;  Uid=traintracker;  Pwd=Yy82!l6X6NU-;  MultipleActiveResultSets=true  ";
+       //Data Source=den1.mssql8.gear.host;Initial Catalog=traintracker;User ID=traintracker
+
+       public static int USerID;
+       public static string USerName;
+       public static bool Admin;
+       public static int distanceOnTrack;
+       public static int routeBordingStation;
+       public static int routeFinalDestination;
+       public static int routeId;
+       public static string setadjoin=null;
+       public static int BoardingStationId;
+       public static int FinalDestinationId;
+       public static int ChooseTrackId;
+       public static int routeIdNew;
+       public static int ScheduleIdMaker;
+       public static string setadjoinCellContentclick;
+       //
+
+       public static int a1d;
+       public static int a2d;
+       public static int a3d;
+       public static int a4d;
+       public static int a5d;
+       public static int a6d;
+       public static int a7d;
+       public static int a8d;
+       public static int a9d;
+       public static int a10d;
+       public static short a1o;
+       public static short a2o;
+       public static short a3o;
+       public static int a4o;
+       public static int a5o;
+       public static int a6o;
+       public static int a7o;
+       public static int a8o;
+       public static int a9o;
+       public static int a10o;
+
+       public static int b1d;
+       public static int b2d;
+       public static int b3d;
+       public static int b4d;
+       public static int b5d;
+       public static int b6d;
+       public static int b7d;
+       public static int b8d;
+       public static int b9d;
+       public static int b10d;
+       public static int b1o;
+       public static int b2o;
+       public static int b3o;
+       public static int b4o;
+       public static int b5o;
+       public static int b6o;
+       public static int b7o;
+       public static int b8o;
+       public static int b9o;
+       public static int b10o;
+
+       public static int c1d;
+       public static int c2d;
+       public static int c3d;
+       public static int c4d;
+       public static int c5d;
+       public static int c6d;
+       public static int c7d;
+       public static int c8d;
+       public static int c9d;
+       public static int c10d;
+       public static int c1o;
+       public static int c2o;
+       public static int c3o;
+       public static int c4o;
+       public static int c5o;
+       public static int c6o;
+       public static int c7o;
+       public static int c8o;
+       public static int c9o;
+       public static int c10o;
+
+       public static int d1d;
+       public static int d2d;
+       public static int d3d;
+       public static int d4d;
+       public static int d5d;
+       public static int d6d;
+       public static int d7d;
+       public static int d8d;
+       public static int d9d;
+       public static int d10d;
+       public static int d1o;
+       public static int d2o;
+       public static int d3o;
+       public static int d4o;
+       public static int d5o;
+       public static int d6o;
+       public static int d7o;
+       public static int d8o;
+       public static int d9o;
+       public static int d10o;
+
+       public static int e1d;
+       public static int e2d;
+       public static int e3d;
+       public static int e4d;
+       public static int e5d;
+       public static int e6d;
+       public static int e7d;
+       public static int e8d;
+       public static int e9d;
+       public static int e10d;
+       public static int e1o;
+       public static int e2o;
+       public static int e3o;
+       public static int e4o;
+       public static int e5o;
+       public static int e6o;
+       public static int e7o;
+       public static int e8o;
+       public static int e9o;
+       public static int e10o;
+
+
+    }
+}
